cd26cf8fe9ae2f1550e00b02a0258e69d0230995
Jack Scarpelli <Festivetomcat99@gmail.com>
2023-01-31 21:07:15 -0500

    commit tree a81c6d9a5693475aa3292e7e35b6bdac66fc8838

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
-}
-
+function createEmployeeRecord(employeeArray) {
+    let employee = {};
+    employee.firstName = employeeArray[0];
+    employee.lastName = employeeArray[1];
+    employee.title = employeeArray[2];
+    employee.payPerHour = parseFloat(employeeArray[3]);
+    employee.timeInEvents = [];
+    employee.timeOutEvents = [];
+    return employee;
+  }
+  
+  function createEmployeeRecords(employeeArrays) {
+    let employeeRecords = [];
+    for (let i = 0; i < employeeArrays.length; i++) {
+      employeeRecords.push(createEmployeeRecord(employeeArrays[i]));
+    }
+    return employeeRecords;
+  }
+  
+  function createTimeInEvent(employeeRecord, dateTimeString) {
+    if (!employeeRecord || !employeeRecord.timeInEvents) return null;
+    let timeInEvent = {};
+    timeInEvent.dateTime = new Date(dateTimeString);
+    employeeRecord.timeInEvents.push(timeInEvent);
+    return timeInEvent;
+  }
+  
+  function createTimeOutEvent(employeeRecord, dateTimeString) {
+    if (!employeeRecord || !employeeRecord.timeOutEvents) return null;
+    let timeOutEvent = {};
+    timeOutEvent.dateTime = new Date(dateTimeString);
+    employeeRecord.timeOutEvents.push(timeOutEvent);
+    return timeOutEvent;
+  }
+  
+  function hoursWorkedOnDate(employeeRecord, date) {
+    if (!employeeRecord || !employeeRecord.timeInEvents || !employeeRecord.timeOutEvents) return 0;
+    let timeInEvents = employeeRecord.timeInEvents.filter(event => event.dateTime.toDateString() === date.toDateString());
+    let timeOutEvents = employeeRecord.timeOutEvents.filter(event => event.dateTime.toDateString() === date.toDateString());
+    let totalHours = 0;
+    for (let i = 0; i < timeInEvents.length; i++) {
+      if (!timeOutEvents[i]) continue;
+      totalHours += (timeOutEvents[i].dateTime - timeInEvents[i].dateTime) / (1000 * 60 * 60);
+    }
+    return totalHours;
+  }
+  
